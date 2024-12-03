bdb72009ab4773243c6060d947eb1057d40c786d
Overtheon <163624752+Overtheon@users.noreply.github.com>
2024-12-03 11:04:53 +0800

    Cubic Spline Interpolation.py

--- preprocessing/interpolation/Cubic Spline Interpolation.py
+++ preprocessing/interpolation/Cubic Spline Interpolation.py
@@ -2,31 +2,20 @@ import pandas as pd
 import numpy as np
 from scipy.interpolate import CubicSpline
 import matplotlib.pyplot as plt
-import datetime
-#读取文件
-df=pd.read_excel("附件1筛选.xlsx",engine='openpyxl')
-df['月份'] = pd.to_datetime(df['月份'],format = '%Y-%m-%d %H:%M:%S')
-print(df)
-for row in df.index:
-    for col in df.columns:
-        if col == '月份' :
-            df.loc[row,col] =df.at[row,'月份'].timestamp()
-x=df.iloc[:,0].values
-y=df.iloc[:,1].values
-print(df)
-'''if not np.all(np.isfinite(x)):
-    print("x 数组中存在非有限值")
-    x=x[np.isfinite(x)]
-    y=y[np.isfinite(x)]
-if not np.all(np.isfinite(y)):
-    print("y 数组中存在非有限值")
-    x=x[np.isfinite(y)]
-    y=y[np.isfinite(y)]'''
-x_interp=np.linspace(min(x),max(x),1000)
-cs=CubicSpline(x,y)
-y_interp=cs(x_interp)
-plt.scatter(x,y,color='red',label='Original Data')
-plt.plot(x_interp,y_interp,label='Cubic Spline')
-plt.legend()
-plt.show()
-print(cs(1664553600))#2022年10月1日00:00:00时间戳
+#"df" refers to the dataframe obtained after importing an Excel file
+def cubic_spline_interpolation(df):
+    #Retrieve columns A and B of the data and convert them into a Numpy array
+    #Column A is x, and column B is y. Column A must be strictly increasing. If column A is not strictly increasing, the data must be sorted first. (If column A contains time-type data, you can convert the time-type data to timestamps before performing interpolation processing)
+    x=df.iloc[:,0].values
+    y=df.iloc[:,1].values
+    #Set the target interpolation points, with "1000" as the number of interpolations
+    x_interp=np.linspace(min(x),max(x),1000)
+    #Perform cubic spline interpolation
+    cs=CubicSpline(x,y)
+    y_interp=cs(x_interp)
+    #Plot the original data points
+    plt.scatter(x,y,color='red',label='Original Data')
+    #Plot the interpolation results
+    plt.plot(x_interp,y_interp,label='Cubic Spline')
+    plt.legend()
+    plt.show()
